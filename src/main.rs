4502f5a930709ec378a3c4b37092631b0c4330f9
j1nxie <rylieeeeexd@gmail.com>
2023-07-10 19:08:13 +0700

    feat: play audio from stream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use stream_download::StreamDownload;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+    let sink = rodio::Sink::try_new(&handle).unwrap();
+
+    let reader =
+        StreamDownload::new_http("https://stream.gensokyoradio.net/3".parse().unwrap()).unwrap();
+
+    sink.append(rodio::Decoder::new(reader).unwrap());
+
+    sink.sleep_until_end();
+
+    Ok(())
 }
